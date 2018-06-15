4a339132fa35d1041f1489b9bc5ddd14e4674ce5
Peter Mechlenborg <peter@humio.com>
2018-06-15 09:55:58 +0200

    Add launch button for eu-west-1

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 cat cloudformation.json | jq
-make build 
+make build
 aws s3 cp --acl public-read cloudformation.json s3://humio-public-us-east-1/
+
 aws s3 cp --acl public-read target/cloudwatch_humio.zip s3://humio-public-eu-central-1/
+aws s3 cp --acl public-read target/cloudwatch_humio.zip s3://humio-public-eu-west-1/
 aws s3 cp --acl public-read target/cloudwatch_humio.zip s3://humio-public-us-east-1/
 aws s3 cp --acl public-read target/cloudwatch_humio.zip s3://humio-public-us-east-2/
+aws s3 cp --acl public-read target/cloudwatch_humio.zip s3://humio-public-us-west-2/
