8d4232c2a280a15dca93d8ea3bb680b9c56feff2
Zanna <suzanna.volkov@hotmail.com>
2020-04-14 08:56:30 +0200

    Consistent use of double quotes instead of mixed single/double quoting

--- lambdas/ingester.py
+++ lambdas/ingester.py
@@ -21,10 +21,10 @@ def setup():
     global humio_ingest_token
     global http_session
 
-    humio_host = os.environ['humio_host']
-    humio_protocol = os.environ['humio_protocol']
-    humio_repository = os.environ['humio_repository_name']
-    humio_ingest_token = os.environ['humio_ingest_token']
+    humio_host = os.environ["humio_host"]
+    humio_protocol = os.environ["humio_protocol"]
+    humio_repository = os.environ["humio_repository_name"]
+    humio_ingest_token = os.environ["humio_ingest_token"]
 
     http_session = requests.Session()
 
@@ -50,17 +50,17 @@ def lambda_handler(event, context):
         setup()
 
     # TODO: Use Python Client.
-    humio_url = '%s://%s/api/v1/dataspaces/%s/ingest' % (humio_protocol, humio_host, humio_repository)
+    humio_url = "%s://%s/api/v1/dataspaces/%s/ingest" % (humio_protocol, humio_host, humio_repository)
     humio_headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer %s' % humio_ingest_token
+        "Content-Type": "application/json",
+        "Authorization": "Bearer %s" % humio_ingest_token
     }
 
     # Decode and unzip the log data.
     decoded_event = helpers.decode_event(event)
 
     # Debug output.
-    print('Event from CloudWatch Logs: %s' % (json.dumps(decoded_event)))
+    print("Event from CloudWatch Logs: %s" % (json.dumps(decoded_event)))
 
     # Extract the general attributes from the event batch.
     batch_attrs = {
@@ -72,20 +72,20 @@ def lambda_handler(event, context):
     }
 
     # Parse out the service name.
-    log_group_parser = re.compile('^/aws/(lambda|apigateway)/(.*)')
-    parsed_log_group = log_group_parser.match(decoded_event.get('logGroup', ''))
+    log_group_parser = re.compile("^/aws/(lambda|apigateway)/(.*)")
+    parsed_log_group = log_group_parser.match(decoded_event.get("", ""))
     if parsed_log_group:
         batch_attrs.update(
             {
-                'awsServiceName': parsed_log_group.group(1),
-                'parsedLogGroupName': parsed_log_group.group(2)
+                "awsServiceName": parsed_log_group.group(1),
+                "parsedLogGroupName": parsed_log_group.group(2)
             }
         )
 
     # Flatten the events from CloudWatch Logs.
     humio_events = []
-    for log_event in decoded_event['logEvents']:
-        message = log_event['message']
+    for log_event in decoded_event["logEvents"]:
+        message = log_event["message"]
 
         # Create the attributes.
         attributes = {}
@@ -94,14 +94,14 @@ def lambda_handler(event, context):
 
         # Append the flattened event
         humio_events.append({
-            'timestamp': log_event['timestamp'],
-            'rawstring': message,
-            'kvparse': True,
-            'attributes': attributes,
+            "timestamp": log_event["timestamp"],
+            "rawstring": message,
+            "kvparse": True,
+            "attributes": attributes,
         })
 
     # Make a batch for the Humio Ingest API.
-    wrapped_data = [{'tags': {'host': 'lambda'}, 'events': humio_events}]
+    wrapped_data = [{"tags": {"host": "lambda"}, "events": humio_events}]
 
     # Make request.
     request = http_session.post(
@@ -113,4 +113,4 @@ def lambda_handler(event, context):
     response = request.text
 
     # Debug output.
-    print('Got response %s from Humio.' % response)
+    print("Got response %s from Humio." % response)
