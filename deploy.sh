29fae37c1ba2db48d8f23eed88c319e4941e9953
Suzanna-Volkov <suzannavolkov@humio.com>
2020-08-03 06:37:07 +0200

    Updated deploy.sh with eu-north-1

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,9 @@ aws s3 cp --acl public-read cloudformation.json s3://humio-public-us-east-1/ --r
 
 aws s3 cp --acl public-read target/cloudwatch_humio.zip s3://humio-public-eu-central-1/ --region eu-central-1
 aws s3 cp --acl public-read target/cloudwatch_humio.zip s3://humio-public-eu-west-1/ --region eu-west-1
+aws s3 cp --acl public-read target/cloudwatch_humio.zip s3://humio-public-eu-west-2/ --region eu-west-2
+aws s3 cp --acl public-read target/cloudwatch_humio.zip s3://humio-public-eu-north-1/  --region eu-north-1
+
 aws s3 cp --acl public-read target/cloudwatch_humio.zip s3://humio-public-us-east-1/ --region us-east-1
 aws s3 cp --acl public-read target/cloudwatch_humio.zip s3://humio-public-us-east-2/ --region us-east-2
 aws s3 cp --acl public-read target/cloudwatch_humio.zip s3://humio-public-us-west-2/ --region us-west-2
-aws s3 cp --acl public-read target/cloudwatch_humio.zip s3://humio-public-eu-west-2/ --region eu-west-2
