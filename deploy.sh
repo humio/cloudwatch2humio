8c45e034b8e3567b5dd5fd0c71a2b6c3b888f02f
Suzanna-Volkov <suzannavolkov@humio.com>
2020-11-06 10:43:58 +0100

    Updated delpoy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ cat cloudformation.json | jq
 make build
 
 aws s3 cp --acl public-read cloudformation.json s3://humio-public-us-east-1/ --region us-east-1
+aws s3 cp --acl public-read cloudformation-no-trail.json s3://humio-public-us-east-1/ --region us-east-1
 
 aws s3 cp --acl public-read target/v1.2.0_cloudwatch2humio.zip s3://humio-public-eu-central-1/ --region eu-central-1
 aws s3 cp --acl public-read target/v1.2.0_cloudwatch2humio.zip s3://humio-public-eu-west-1/ --region eu-west-1
