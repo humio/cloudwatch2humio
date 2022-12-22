e108103e6fac909a1a33ef36baba0a13adf6a21d
Zanna <suzanna.volkov@crowdstrike.com>
2022-12-22 11:31:22 +0100

    Removed versioning deployment scripts and Makefile

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,10 @@ set -e
 cat cloudformation.json | jq
 make build
 
-# It is not necessary to upload the lambda code files to anywhere but the us-east-1 region now as the files
-# will be retrieved from there no matter what region the CloudStack.
+# It is not necessary to upload the Lambda code files to anywhere but the us-east-1 region now as the files
+# will be retrieved from there no matter what region the CloudStack is created in.
+# The commands are kept here to keep an overview of where there are buckets created for this integration as
+# they should probably be removed in the future.
 
 aws s3 cp --acl public-read cloudformation.json s3://humio-public-us-east-1/ --region us-east-1
 #aws s3 cp --acl public-read cloudformation-no-trail.json s3://humio-public-us-east-1/ --region us-east-1
@@ -15,7 +17,7 @@ aws s3 cp --acl public-read cloudformation.json s3://humio-public-us-east-1/ --r
 #aws s3 cp --acl public-read target/v1.2.2_cloudwatch2humio.zip s3://humio-public-eu-west-3/ --region eu-west-3
 #aws s3 cp --acl public-read target/v1.2.2_cloudwatch2humio.zip s3://humio-public-eu-north-1/  --region eu-north-1
 
-aws s3 cp --acl public-read target/v1.3.0_cloudwatch2humio.zip s3://humio-public-us-east-1/ --region us-east-1
+aws s3 cp --acl public-read target/cloudwatch2humio.zip s3://humio-public-us-east-1/ --region us-east-1
 #aws s3 cp --acl public-read target/v1.2.2_cloudwatch2humio.zip s3://humio-public-us-east-2/ --region us-east-2
 #aws s3 cp --acl public-read target/v1.2.2_cloudwatch2humio.zip s3://humio-public-us-west-2/ --region us-west-2
 
